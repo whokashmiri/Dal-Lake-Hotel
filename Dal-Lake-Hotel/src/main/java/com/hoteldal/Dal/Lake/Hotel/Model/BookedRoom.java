d100cc78b82532979d7d16ed3b43fd9f7aeaac48
Aaqib Mir <67498640+whokashmiri@users.noreply.github.com>
2025-01-11 08:41:54 +0530

    working on Entities

--- Dal-Lake-Hotel/src/main/java/com/hoteldal/Dal/Lake/Hotel/Model/BookedRoom.java
+++ Dal-Lake-Hotel/src/main/java/com/hoteldal/Dal/Lake/Hotel/Model/BookedRoom.java
@@ -1,18 +1,46 @@
 package com.hoteldal.Dal.Lake.Hotel.Model;
 
-import java.time.LocalDate;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.apache.logging.log4j.util.Lazy;
 
+import java.time.LocalDate;
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class BookedRoom {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "check_in")
     private LocalDate checkInDate;
+    @Column(name = "check_out")
     private LocalDate checkOutDate;
+    @Column(name = "guest_fullName")
     private  String guestFullName;
+    @Column(name = "guest_email")
     private  String guestEmail;
+    @Column(name = "adults")
     private  int numOfAdults;
+    @Column(name = "children")
     private  int numOfChildren;
+    @Column(name = "total_guests")
     private  int totalNumOfGuest;
+    @Column(name = "comfirmation_code")
     private String bookingConfirmationCode;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id")
+
     private Room room;
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
 
     public void calculateTotalNumberOfGuests(){
         this.totalNumOfGuest = this.numOfAdults +this.numOfChildren;
@@ -32,4 +60,6 @@ public class BookedRoom {
     public void setBookingConfirmationCode(String bookingConfirmationCode) {
         this.bookingConfirmationCode = bookingConfirmationCode;
     }
+
+
 }
